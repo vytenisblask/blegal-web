1dc441d466f7706aad7bf24d404e3d4c7511bb0c
vytenisblask <vytenisb@yahoo.com>
2023-09-28 13:21:24 +0300

    front page updated to include 4 selected posts

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <!-- template parts -->
 <?php get_template_part('template-parts/hero'); ?>
 
+<?php get_template_part('template-parts/content', 'paslaugos'); ?>
 
 <?php get_template_part('template-parts/get-in-touch'); ?>
 <?php get_footer(); ?>
--- template-parts/get-in-touch.php
+++ template-parts/get-in-touch.php
@@ -1,4 +1,3 @@
-<!-- get-in-touch.php -->
 <div class="get-in-touch-container">
     <div class="content">
         <p class="text">Esame įsitikinę, jog profesionalios teisinės paslaugos gali tapti esminiu Jūsų verslo konkurenciniu pranašumu rinkoje.</p>
